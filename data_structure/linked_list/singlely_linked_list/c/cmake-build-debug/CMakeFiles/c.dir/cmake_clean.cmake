6e5662a5a90d3076fe053698ea8afb4b03044951
Sudo <sudharsansci@gmail.com>
2021-06-06 03:11:41 -0400

    Restructure Folder
    Signed-off-by: Sudharsan Balasubramani <sudharsansci@gmail.com>

---
+++ data_structure/linked_list/singlely_linked_list/c/cmake-build-debug/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/main.c.obj"
+  "c.exe"
+  "c.exe.manifest"
+  "c.lib"
+  "c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
