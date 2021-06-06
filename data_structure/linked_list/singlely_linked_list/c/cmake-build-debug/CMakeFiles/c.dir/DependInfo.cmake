ab5dc94d6b20d8f56a961a4a7a06ba80d0bfee3c
Sudo <sudharsansci@gmail.com>
2021-06-06 01:05:29 -0400

    Singlely linked list
    Signed-off-by: Sudharsan Balasubramani <sudharsansci@gmail.com>

---
+++ data_structure/linked_list/singlely_linked_list/c/cmake-build-debug/CMakeFiles/c.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "G:/Sudo/Workshop/Repo Maintanence/IamProgrammer/data_structure/linked_list/singlely_linked_list/c/main.cpp" "G:/Sudo/Workshop/Repo Maintanence/IamProgrammer/data_structure/linked_list/singlely_linked_list/c/cmake-build-debug/CMakeFiles/c.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ data_structure/linked_list/singlely_linked_list/c/cmake-build-debug/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/main.cpp.obj"
+  "c.exe"
+  "c.exe.manifest"
+  "c.lib"
+  "c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
